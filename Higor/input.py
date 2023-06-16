cedfba744ca53a49a343dd06e9d68fd072fc836a
HigorHD <higor@habeasdata.com.br>
2023-06-16 15:54:21 -0300

    program com input de número

---
+++ Higor/input.py
@@ -0,0 +1,8 @@
+
+
+numero = input("Digite um número: ")
+
+if float(numero) % 2 ==0:
+    print("o númera digitado foi :" + numero + " e é par")
+else:
+    print("o númera digitado foi :" + numero + " e é impar")
