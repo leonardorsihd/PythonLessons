d4c16f11ba3942ba332a6a14c1d0c82a913ea0cb
leonardorsihd <leonardorsihd@gmail.com>
2023-06-15 22:01:29 -0300

    Atualizando diretorios

--- Higor/hello.py
+++ Higor/hello.py
@@ -1,1 +1,11 @@
 print ('hello world my friend')
+
+fruta = input("Digite o nome da fruta: ")
+
+if fruta == "maça":
+    print("1")
+elif fruta == "melao":
+    print("2")
+else:
+    print("banana")
+
